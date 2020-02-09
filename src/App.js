f45d06ef5584434fa657cc5e1c1aec3b51380bd4
George <george@gmail.com>
2020-02-09 07:32:42 -0500

    initial components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+// import logo from "./logo.svg";
+import { CardList } from "./components/card-list/card-list.component";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+
+class App extends Component {
+  state = {
+    monsters: [
+      { name: "Frankenstein", id: 1 },
+      { name: "Dracula", id: 2 },
+      { name: "Zombie", id: 3 }
+    ]
+  };
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => {
+        this.setState({ monsters: users });
+        users.forEach(user => {
+          console.log(user.name);
+        });
+      });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <CardList name={"HOLA"}>
+          {this.state.monsters.map(monster => (
+            <h1 key={monster.id}>
+              {monster.name}
+              {monster.email}
+            </h1>
+          ))}
+        </CardList>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import "src/css/components/card-list.styles.css";
+
+export const CardList = props => {
+  return <div>{props.children}</div>;
+};
