ec07a93160e0a1d4fe1ffcd66387d9ac07a01085
George <george.kurguzov@gmail.com>
2020-02-09 16:05:28 -0500

    Updates

---
+++ precache-manifest.47e6ef67893f983f3bddcfc2ab976673.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea913a6da6a8047b7df2023a4a366509",
+    "url": "/robots-rolodex/index.html"
+  },
+  {
+    "revision": "fd0cd9b22040ca5f4beb",
+    "url": "/robots-rolodex/static/css/main.0f173e88.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/robots-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/robots-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd0cd9b22040ca5f4beb",
+    "url": "/robots-rolodex/static/js/main.65e24eeb.chunk.js"
+  },
+  {
+    "revision": "2ad57ae96986df441f85",
+    "url": "/robots-rolodex/static/js/runtime-main.05f4a127.js"
+  }
+]);
